527cd07266b6bbb09c9f361d84bc7cdb9ce347a5
Ezequiel Mella Leonardelli <emleonardelli@gmail.com>
2020-02-01 07:15:08 -0300

    DB 1

--- config.js
+++ config.js
@@ -4,5 +4,11 @@ module.exports = {
   },
   jwt: {
     secret: process.env.JWT_SECRET || '123456'
+  },
+  mysql: {
+    host: process.env.MYSQL_HOST || 'localhost',
+    user: process.env.MYSQL_USER || 'ezequiel',
+    password: process.env.MYSQL_PASS || 'no11lo22',
+    database: process.env.MYSQL_DB || 'platzi_nodejs',
   }
 }
