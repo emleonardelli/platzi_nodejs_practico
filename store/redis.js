21fe326f83f38519f2786389d3fe3c4f277ae0ac
Ezequiel Mella Leonardelli <emleonardelli@gmail.com>
2020-02-07 18:47:50 -0300

    Redis cache

---
+++ store/redis.js
@@ -0,0 +1,37 @@
+const redis = require('redis')
+const client = redis.createClient()
+
+function list(table){
+  return new Promise((resolve, reject) => {
+    client.get(table, (err, data) =>{
+      if (err) {
+        return reject(err)
+      }
+      let res=data || null
+      if (data) {
+        res=JSON.stringify(data)
+      }
+      resolve(res)
+    })
+  })
+}
+
+function get(table, id){
+  
+}
+
+async function upsert(table, data){
+  let key = table
+  if (data && data.id){
+    key = key + '_' + data.id
+  }
+
+  client.setex(key, 10, JSON.stringify(data))
+  return true
+}
+
+module.exports = {
+  list, 
+  get, 
+  upsert
+}
